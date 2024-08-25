b35ed7eefaaf1d103913a36aca390d41e33dc8fc
Kevin Garcia <kggc03@gmail.com>
2024-08-24 23:32:52 -0600

    Initial Commit

---
+++ EducAR/EducARApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducARApp.swift
+//  EducAR
+//
+//  Created by Kevin Garcia on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct EducARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
